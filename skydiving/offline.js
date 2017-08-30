bec4ea3502ea32c098890220f2768a3cb4ef7654
erick-bergamini <erick.bergamini@gmail.com>
2017-08-29 21:26:20 -0300

    Add files via upload

---
+++ skydiving/offline.js
@@ -0,0 +1,1 @@
+{"version":1504032681716,"fileList":["c2runtime.js","data.js","start.js","style.css","offlineClient.js","images/shared-0-sheet0.png","images/tiledbackground-sheet0.png","icons/icon-16.png","icons/loading-logo.png","icons/icon-32.png","icons/icon-64.png","icons/icon-128.png","icons/icon-256.png","icons/icon-512.png","register-sw.js"]}
